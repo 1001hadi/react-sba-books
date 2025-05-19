33eed500dabf055f16b122e516c786956f1f209c
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-19 13:43:45 -0600

    boillerplate set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="container">
+      <h1>Search your favorite Books here!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
